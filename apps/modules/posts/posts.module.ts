255d01cafa0bc6aaa923ee241ef1fd81becc2584
Daniel Bello <gokebello@gmail.com>
2025-03-31 18:34:25 -0400

    chore: updates

--- apps/modules/posts/posts.module.ts
+++ apps/modules/posts/posts.module.ts
@@ -2,19 +2,18 @@ import { DataSource, Repository } from "typeorm";
 import { PostController } from "./posts.controller";
 import { PostService } from "./posts.service";
 import { Post } from "./entities/post.entity";
-import { StorageService } from "@/libs/storage/storage.service";
-import { StorageModule } from "@/libs/storage/storage.module";
+import { FileModule } from "../files";
 
 export class PostsModule {
 	public service: PostService;
 	public controller: PostController;
 	public repo: Repository<Post>;
-	public storage: StorageModule;
+	public files: FileModule;
 
 	constructor(datasource: DataSource) {
 		this.repo = datasource.getRepository<Post>(Post);
-		this.storage = new StorageModule();
-		this.service = new PostService(this.repo, this.storage.service);
+		this.files = new FileModule(datasource);
+		this.service = new PostService(this.repo, this.files.service);
 		this.controller = new PostController(this.service);
 	}
 }
