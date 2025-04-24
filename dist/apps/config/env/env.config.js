af32f2888233c8c831672f341910cf37d6695502
Daniel Bello <gokebello@gmail.com>
2025-04-24 05:54:47 -0400

    chore: updates

--- dist/apps/config/env/env.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = exports.EXT = exports.NODE_ENV = void 0;
-exports.NODE_ENV = (_a = process.env.NODE_ENV) !== null && _a !== void 0 ? _a : "production";
-exports.EXT = exports.NODE_ENV == "development" ? "DEV" : "PRO";
-exports.ENV = exports.NODE_ENV == "development" ? "dev" : "pro";
-//# sourceMappingURL=env.config.js.map
